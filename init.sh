348f433c3eca1571dbb084c33fd60526b5b448d5
squizlee <ethanscavia777@gmail.com>
2024-11-19 21:08:31 +1100

    init script

--- init.sh
+++ init.sh
@@ -4,3 +4,8 @@ echo "COPYING SHELL SCRIPTS"
 mkdir -p "$HOME/.local/bin"
 cp -r shell/. "$HOME/.local/bin"
 
+echo "COPYING CONFIGURATIONS"
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.config/nvim"
+cp neovim/init.vim "$HOME/.config/nvim"
+
